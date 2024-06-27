c9ec8ee49c4305b7165c72eab06655dcf9dcd1f6
ohendarko <ohendarko@gmail.com>
2024-06-27 08:06:18 +0000

    island perimeter

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+returns the perimeter of the island described in grid
+"""
+
+
+def island_perimeter(grid):
+    """returns the perimeter of the island described in grid"""
+    if not grid or not grid[0]:
+        return 0
+
+    rows, cols = len(grid), len(grid[0])
+    perimeter = 0
+
+    for r in range(rows):
+        for c in range(cols):
+            if grid[r][c] == 1:
+                # Check each of the four directions
+                if r == 0 or grid[r - 1][c] == 0:  # Up
+                    perimeter += 1
+                if r == rows - 1 or grid[r + 1][c] == 0:  # Down
+                    perimeter += 1
+                if c == 0 or grid[r][c - 1] == 0:  # Left
+                    perimeter += 1
+                if c == cols - 1 or grid[r][c + 1] == 0:  # Right
+                    perimeter += 1
+
+    return perimeter
